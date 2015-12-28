f1cd6f7b24e67082126fae67d54492becef21666
hell03610 <eraseunavez@gmail.com>
2015-12-28 13:13:19 +0100

    Show addon on meetup event details

---
+++ background.js
@@ -0,0 +1,15 @@
+var ruleMeetup = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { hostEquals: 'www.meetup.com' },
+      css: ["#eventdets"]
+    })
+  ],
+  actions: [ new chrome.declarativeContent.ShowPageAction() ]
+};
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([ruleMeetup]);
+  });
+});
